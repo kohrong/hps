3dfc387ac96df5af8b2007672b06e98b6ef54497
Josue <Josue@10.230.169.218>
2013-02-06 10:16:00 +0000

    First Commit

---
+++ src/arbolsintactico/ArbolSintactico.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolsintactico;
+
+/**
+ *
+ * @author Josue
+ */
+public class ArbolSintactico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nodo operador1 = new Nodo(1);
+    }
+}
---
+++ src/arbolsintactico/Nodo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolsintactico;
+
+/**
+ *
+ * @author Josue
+ */
+public class Nodo {
+
+    
+    
+    Nodo(int i) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
